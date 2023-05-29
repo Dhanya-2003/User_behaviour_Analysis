15d874b71e0ae2058a181a9b432c5ae73eedb6a3
Dhanya-2003 <53526802+Dhanya-2003@users.noreply.github.com>
2023-05-29 15:17:05 +0530

    Add files via upload

---
+++ apps/authentication/urls.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.urls import path
+from .views import login_view, register_user
+from apps.home import views
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('login/', login_view, name="login"),
+    path('register/', register_user, name="register"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    # path('my-table/', views.my_view, name='my_view'),
+    path('add_rows/', views.add_rows, name='add_rows')
+]
